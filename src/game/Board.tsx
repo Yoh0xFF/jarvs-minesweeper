827b6ab4c05df3bb0d51fb409b26deca1a98f80e
Ioram Gordadze <Yoh0xFF@gmail.com>
2022-06-17 00:20:09 +0100

    Working on board layout.

--- src/game/Board.tsx
+++ src/game/Board.tsx
@@ -5,23 +5,34 @@ import Tile from './Tile';
 
 export default function Board() {
   return (
-    <>
-      <div className='board'>
+    <div className='game-container'>
+      <div className='game-header'>
         <div className='corner-top-left' />
-        <div className='header border-horizontal' />
+        <div className='top-border border-horizontal' />
         <div className='corner-top-right' />
 
-        <div className='left border-vertical' />
-        <div className='right border-vertical' />
+        <div className='face face-unpressed' />
+
+        <div className='left-border border-vertical' />
+        <div className='right-border border-vertical' />
+      </div>
+
+      <div className='game-board'>
+        <div className='joint-left' />
+        <div className='top-border border-horizontal' />
+        <div className='joint-right' />
+
+        <div className='left-border border-vertical' />
+        <div className='right-border border-vertical' />
 
         {[...Array(10)].map((_, i) =>
           [...Array(10)].map((_, j) => <Tile x={i} y={j} />)
         )}
 
         <div className='corner-bottom-left' />
-        <div className='footer border-horizontal' />
+        <div className='bottom-border border-horizontal' />
         <div className='corner-bottom-right' />
       </div>
-    </>
+    </div>
   );
 }
