a938011adf55c6e6ce7797f9315a5ad755639762
Ioram Gordadze <Yoh0xFF@gmail.com>
2022-07-02 17:45:02 +0100

    Add shake animation.

--- src/game-ui/Game.tsx
+++ src/game-ui/Game.tsx
@@ -47,6 +47,7 @@ export default function Game() {
       />
 
       <GameLayout
+        gameStatus={state.gameStatus}
         controls={
           <Controls
             gameStatus={state.gameStatus}
--- src/game-ui/GameLayout.tsx
+++ src/game-ui/GameLayout.tsx
@@ -1,15 +1,22 @@
+import classNames from 'classnames';
+import { GameStatus } from 'game-logic/types';
 import { ReactNode } from 'react';
 
 import styles from './GameLayout.module.scss';
 
 interface Props {
+  gameStatus: GameStatus;
   controls: ReactNode;
   cellsGrid: ReactNode;
 }
 
-export default function GameLayout({ controls, cellsGrid }: Props) {
+export default function GameLayout({ gameStatus, controls, cellsGrid }: Props) {
   return (
-    <div className={styles.layout}>
+    <div
+      className={classNames(styles.layout, {
+        [styles.shake]: gameStatus === 'Fail',
+      })}
+    >
       <section>
         <div>{controls}</div>
       </section>
