fbbd67eca62b7f27fef882cfba5ba65f5b14c082
Ioram Gordadze <Yoh0xFF@gmail.com>
2022-07-05 23:55:51 +0100

    Add confetti animation on success.

--- src/game-ui/GameLayout.tsx
+++ src/game-ui/GameLayout.tsx
@@ -1,6 +1,7 @@
+import confetti from 'canvas-confetti';
 import classNames from 'classnames';
 import { GameStatus } from 'game-logic/types';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 
 import styles from './GameLayout.module.scss';
 
@@ -10,7 +11,46 @@ interface Props {
   cellsGrid: ReactNode;
 }
 
+function _fire(particleRatio: number, opts: confetti.Options) {
+  confetti({
+    ...opts,
+    origin: { y: 0.7 },
+    particleCount: Math.floor(200 * particleRatio),
+  });
+}
+
+function _runConfettiAnimation() {
+  _fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+  });
+  _fire(0.2, {
+    spread: 60,
+  });
+  _fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8,
+  });
+  _fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2,
+  });
+  _fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+  });
+}
+
 export default function GameLayout({ gameStatus, controls, cellsGrid }: Props) {
+  useEffect(() => {
+    if (gameStatus !== 'Success') return;
+
+    _runConfettiAnimation();
+  }, [gameStatus]);
+
   return (
     <div
       className={classNames(styles.layout, {
